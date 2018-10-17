d2505418b62df687725e20432ef5e7ec402f4022
chenfeng.jia <chenfeng.jia@bitmain.com>
2018-10-17 11:03:34 +0800

    接口提取

--- src/views/api.js
+++ src/views/api.js
@@ -1,22 +1,12 @@
 export default [
   {
     name: 'login', // 登录
-    url: '/uaa/oauth/token',
-    methods: ['post', 'get']
-  },
-  {
-    name: 'logouting', // 登出
-    url: '/uaa/logouting',
-    methods: ['get']
+    url: '/api-open/login',
+    methods: ['post']
   },
   {
     name: 'checkValidateCode', // 校验验证码
     url: '/uaa/checkImageCode',
-    methods: ['get']
-  },
-  {
-    name: 'userInfo', // 用户信息
-    url: '/uaa/userinfo',
     methods: ['get']
   },
   {
